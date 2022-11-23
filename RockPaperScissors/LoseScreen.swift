c4ac5e1dfe50bef13e1871a1b6f8e905320e6a6f
christian <christian.bourquin23@d155.org>
2022-11-22 19:57:25 -0600

    newRockPaperScissors

---
+++ RockPaperScissors/LoseScreen.swift
@@ -0,0 +1,29 @@
+//
+//  LoseScreen.swift
+//  RockPaperScissors
+//
+//  Created by Jordan Kramer on 11/21/22.
+//
+
+import UIKit
+
+class LoseScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RockPaperScissors/WinScreen.swift
@@ -0,0 +1,29 @@
+//
+//  WinScreen.swift
+//  RockPaperScissors
+//
+//  Created by Jordan Kramer on 11/21/22.
+//
+
+import UIKit
+
+class WinScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
